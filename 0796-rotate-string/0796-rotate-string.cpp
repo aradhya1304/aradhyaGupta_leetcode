22388c4b9f3499b39e0a32d1f0cc79562f6c0826
aradhya1304 <2k23.cscys2311378@gmail.com>
2026-03-23 22:52:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) return false;
+    bool rotateString(string& s, string& goal) {
 
-        for (int i = 0; i < s.size(); i++) {
-            char first = s[0];
-            s = s.substr(1) + first;
-
-            if (s == goal) return true;
+        if (s.length() != goal.length()) {
+            return false;
         }
-        return false;
+
+        string doubledS = s + s;
+
+        return doubledS.find(goal) != string::npos;
     }
 };
